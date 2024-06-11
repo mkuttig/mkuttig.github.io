b96b6844a9109a343083e2cf47f3a9a0e8be6a50
mkuttig <mathias@kuttig-engineering.de>
2024-06-11 12:17:30 +0200

    Set variables

--- app.js
+++ app.js
@@ -129,7 +129,7 @@ class App{
         if (this.bell) {
             //this.bell.rotateY(this.joy1_x);
             //this.bell.position.set( this.heli_x, this.heli_y, this.heli_z);            
-            this.bell.position.set( this.heli_x, this.heli_y, this.heli_z);
+            this.bell.position.set( 0, 2, 0);
 
         }
         
